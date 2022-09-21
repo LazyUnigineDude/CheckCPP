c9f5504f20e43c6fa87ac63370540352c3665d66
LazyUnigineDude <lazy.unigine@gmail.com>
2022-09-21 16:15:46 -0400

    List of things to learn

--- CheckCPP.cpp
+++ CheckCPP.cpp
@@ -1,21 +1,54 @@
-#include <iostream>
-
+﻿#include <iostream>
 
 // Call a header and use a function;
 // #include "OOPCheck.h"
+#include <unordered_map>
+
+class Check
+{
+public:
+	Check();
+	~Check();
+	Check(const char* y, int x) { this->x = x; this->y = y; Map[y] = x; }
+
+	std::unordered_map<const char*, int> Map;
+
+private:
+	int x;
+	const char* y;
+
+};
+
+Check::Check() : x(0), y("")
+{
+}
+
+Check::~Check()
+{
+}
+
 
 int main() { 
 
+	Check C("HI", 15), D("Hello",2);
+	const char* x = "HI";
+	std::cout << C.Map[x] << "\n";
 
 
-
-
-
-
-
-
-
-
+	/*
+		Arrays
+		Lists
+		Linked List
+		Doubly Linked Lists
+		Vector
+		Stacks
+		Queues
+		Hashtables
+		Maps
+		Graphs
+	*/
+	// Some example sentence over here  x;  Time = x; Space = 26
+	//									    Time =  x + 1; Space = unique letter
 
 	return 0;
 }
