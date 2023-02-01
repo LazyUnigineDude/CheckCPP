25dc80f77d7b84afda4b68b9b95de5ecfa259e61
LazyUnigineDude <lazy.unigine@gmail.com>
2023-02-01 17:40:20 -0500

    sf

--- Multithreading.h
+++ Multithreading.h
@@ -50,7 +50,7 @@ class Multithreading
 {
 public:
 
-	//void MainProg();
+	void MainProg();
 	void Sender();
 
 private:
@@ -102,86 +102,86 @@ void Multithreading::Worker() {
 }
 
 
-//void Multithreading::MainProg() {
-//
-//	//srand(time(0));
-//	//for (int i = 0; i < 10; i++){std::cout << (1 + rand()) % 10 << std::endl;}
-//
-//	char y = 'y';
-//
-//	// Vector Threads
-//	std::vector<std::thread> VThread;
-//	VThread.push_back(std::thread(F1));
-//
-//	// Pass by reference
-//	VThread.push_back(std::thread(std::ref(F2), y));
-//
-//	for (auto& T : VThread) {
-//		if (T.joinable())
-//			T.join();
-//	}
-//
-//	// Function Threads
-//	std::thread Check1(F1);
-//	std::thread Check2(F2, 'y');
-//	Check1.join();
-//	Check2.join();
-//
-//
-//	// Functor Threads using operator
-//	//std::unique_ptr<CheckThread> X = std::make_unique<CheckThread>();
-//	CheckThread* FunctorThread = new CheckThread();
-//	std::thread FThread(std::ref(*FunctorThread), 5, 3);
-//	FThread.join();
-//
-//
-//	// Lambda Threads
-//	std::thread LambdaCheck([]() {for (int x = 0; x < 15; x++) { std::cout << x << "\n"; }; });
-//	LambdaCheck.join();
-//
-//	// ASync
-//	auto t = std::async(Total, 5, 6);
-//	std::cout << "Total is: " << t.get() << "\n";
-//
-//
-//	//Mutex lock and unlock Fnc
-//	int _y = 0;
-//	MutexLock(_y);
-//	std::mutex Mutex;
-//
-//	//			SMART	LOCKS
-//	///////////////////////////////////////////////
-//	{
-//		// Default Lock/ Unlock via Scope end
-//		std::lock_guard<std::mutex> LockGuard(Mutex);
-//		_y++;
-//	}
-//
-//	{
-//		// Choice Lock/ Unlock via Scope end
-//		std::unique_lock<std::mutex> LockGuard(Mutex);
-//		_y++;
-//		LockGuard.unlock();
-//	}
-//
-//	//	<shared_mutex>		SHARED	LOCKS
-//	////////////////////////////////////////////////
-//	std::shared_mutex MutexShared;
-//	{
-//		std::unique_lock<std::shared_mutex> SharedLock(MutexShared);
-//		_y++;
-//	}
-//	{
-//		std::shared_lock<std::shared_mutex> WatchShared(MutexShared);
-//		std::cout << _y;
-//
-//	}
-//
-//	// Deadlock Fix
-//	///////////////////////////////////////////////
-//	{
-//		std::mutex G1, G2;
-//		std::scoped_lock lock(G1,G2);
-//	}
-//
-//}
+void Multithreading::MainProg() {
+
+	//srand(time(0));
+	//for (int i = 0; i < 10; i++){std::cout << (1 + rand()) % 10 << std::endl;}
+
+	char y = 'y';
+
+	// Vector Threads
+	std::vector<std::thread> VThread;
+	VThread.push_back(std::thread(F1));
+
+	// Pass by reference
+	VThread.push_back(std::thread(std::ref(F2), y));
+
+	for (auto& T : VThread) {
+		if (T.joinable())
+			T.join();
+	}
+
+	// Function Threads
+	std::thread Check1(F1);
+	std::thread Check2(F2, 'y');
+	Check1.join();
+	Check2.join();
+
+
+	// Functor Threads using operator
+	//std::unique_ptr<CheckThread> X = std::make_unique<CheckThread>();
+	CheckThread* FunctorThread = new CheckThread();
+	std::thread FThread(std::ref(*FunctorThread), 5, 3);
+	FThread.join();
+
+
+	// Lambda Threads
+	std::thread LambdaCheck([]() {for (int x = 0; x < 15; x++) { std::cout << x << "\n"; }; });
+	LambdaCheck.join();
+
+	// ASync
+	auto t = std::async(Total, 5, 6);
+	std::cout << "Total is: " << t.get() << "\n";
+
+
+	//Mutex lock and unlock Fnc
+	int _y = 0;
+	MutexLock(_y);
+	std::mutex Mutex;
+
+	//			SMART	LOCKS
+	///////////////////////////////////////////////
+	{
+		// Default Lock/ Unlock via Scope end
+		std::lock_guard<std::mutex> LockGuard(Mutex);
+		_y++;
+	}
+
+	{
+		// Choice Lock/ Unlock via Scope end
+		std::unique_lock<std::mutex> LockGuard(Mutex);
+		_y++;
+		LockGuard.unlock();
+	}
+
+	//	<shared_mutex>		SHARED	LOCKS
+	////////////////////////////////////////////////
+	std::shared_mutex MutexShared;
+	{
+		std::unique_lock<std::shared_mutex> SharedLock(MutexShared);
+		_y++;
+	}
+	{
+		std::shared_lock<std::shared_mutex> WatchShared(MutexShared);
+		std::cout << _y;
+
+	}
+
+	// Deadlock Fix
+	///////////////////////////////////////////////
+	{
+		std::mutex G1, G2;
+		std::scoped_lock lock(G1,G2);
+	}
+
+}
