a475fd045bd5f12f1f5993c0476591e2b19f8697
LazyUnigineDude <lazy.unigine@gmail.com>
2023-02-05 23:18:19 -0500

    fix

--- CheckCPP.cpp
+++ CheckCPP.cpp
@@ -1,12 +1,10 @@
 ﻿#include <iostream>
-#include "Multithreading.h"
 
 
 
 int main() {
 
-    //vector<vector<string>> paths = { {"London", "New York"} ,{"New York", "Lima"},{"Lima", "Sao Paulo" }};
-  //  vector<vector<int>> grids = { {0,0,1,1},{1,0,1,0},{1,1,0,0} };
-  //  std::cout << matrixScore(grids);
+   
+
 
 }
