a7ffd7e4250c44b923e3a76d709f666e99800acd
LazyUnigineDude <lazy.unigine@gmail.com>
2023-02-06 20:11:19 -0500

    Add fold expressions

--- CheckCPP.cpp
+++ CheckCPP.cpp
@@ -1,10 +1,12 @@
 ﻿#include <iostream>
-
+#include "FoldExpressions.h"
 
 
 int main() {
 
-   
+	//Print("5", 135, "Idks ", 1, "Yeah\n" , 15);
 
-
+	//std::cout << findTotal(10, 2, 3, 4, 5, 6) << "\n";
+	std::cout << minusOne(10, 15, -20);
+	return 0;
 }
--- FoldExpressions.h
+++ FoldExpressions.h
@@ -8,9 +8,39 @@ T findTotal(T lastone) {
 }
 
 template <typename T, typename ...p>
-T findTotal(T head, p ...tail) { // fold expressions
+T findTotal(T head, p ...tail) { 
+    // variyadic template list
 
     if (sizeof...(tail))
         head += findTotal(tail...);
     return head;
+}
+
+template <typename ...T>
+auto sum(T ...p) {
+    return (p + ...);
+    // pack op ...
+    // Unurary Left fold
+}
+
+template <typename ...T>
+auto sum2(T ...p) {
+    return (... + p);
+    //      ... op pack
+    // Uni Right Fold
+}
+
+template <typename ...list >
+void Print(list ...args) {
+    (std::cout << ... << args) << "\n";
+  //    INIT   op ... op pack
+    // Binary left fold
+}
+
+template <typename ... T>
+auto minusOne(T ... p)
+{
+    return (p - ... - 0);
+  //    pack op ... op INIT
+    // Binary Right fold
 }
