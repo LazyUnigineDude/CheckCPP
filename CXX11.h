abcdb150ce8b5e6dfbf543ee918adc767a777648
LazyUnigineDude <lazy.unigine@gmail.com>
2023-02-08 14:42:23 -0500

    Add

--- CXX11.h
+++ CXX11.h
@@ -6,20 +6,20 @@ struct CXX11 {
 
 	CXX11() {
 		std::vector<std::string> Items = {
-		"auto, decltype, final, delete, override, nullptr, union, long long, char16_t && char32_t",
-		"constexp, noexcept, alignof, alignas, for(auto &i : val)",
-		"R value references [ type Obj = &&value ]",
-		"Move constructors && Assignment Operators [ &class::operator=(class&& param) ]",
-		"Scoped Enum [ enum class Obj : type { value = 10, value2 = 2 }; ]",
-		"List Init [ std::list<type> Name = { value, value }; ]",
-		"Delegate constructors [ struct S{ S(int x, char y) {} S(int x) : S (x, 'x'){} } ]",
-		"Aliases && Templates [ using name = {};  template <typename T> void foo(T t){} ]",
-		"Variyadic Templates [ template<typename ...T> void foo(T ...t) {} ]",
-		"Literals [ char16_t* x = u\"String\" ]",
-		"Attributes [ [[noreturn]] void f() {throw \"error\";} ]",
-		"Lambda expressions [ [](){}; ]",
-		"Smart Pointers [ std::unique_ptr ]",
-		"Multithreading [ std::thread, std::atomic, std::mutex ]\n"
+			"auto, decltype, final, delete, override, nullptr, union, long long, char16_t && char32_t",
+			"constexp, noexcept, alignof, alignas, for(auto &i : val)",
+			"R value references [ type Obj = &&value ]",
+			"Move constructors && Assignment Operators [ &class::operator=(class&& param) ]",
+			"Scoped Enum [ enum class Obj : type { value = 10, value2 = 2 }; ]",
+			"List Init [ std::list<type> Name = { value, value }; ]",
+			"Delegate constructors [ struct S{ S(int x, char y) {} S(int x) : S (x, 'x'){} } ]",
+			"Aliases && Templates [ using name = {};  template <typename T> void foo(T t){} ]",
+			"Variyadic Templates [ template<typename ...T> void foo(T ...t) {} ]",
+			"Literals [ char16_t* x = u\"String\" ]",
+			"Attributes [ [[noreturn]] void f() {throw \"error\";} ]",
+			"Lambda expressions [ [](){}; ]",
+			"Smart Pointers [ std::unique_ptr ]",
+			"Multithreading [ std::thread, std::atomic, std::mutex ]\n"
 		};
 
 		std::cout << "List of Stuff:\n\n";
