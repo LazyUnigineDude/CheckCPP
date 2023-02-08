d8d660311a584d7e45dc61bff2550d6371349e65
LazyUnigineDude <lazy.unigine@gmail.com>
2023-02-08 17:18:02 -0500

    Add

--- CXX20.h
+++ CXX20.h
@@ -7,11 +7,20 @@ struct CXX20 {
 
 	CXX20() {
 		std::vector<std::string> Items = {
-			" "
+			"consteval, constinit expressions",
+			"3 Way Comparison Operator (SpaceShip Operator) [ <=> ]",
+			"Default Equal Operator[ operator==() = default ]",
+			"Designated Inits [ struct S{int x, y, z}; S obj{ .x = 1, .z = 2 /*.y auto 0*/ }; ]",
+			"Ranged For init clause [ for(int x = 0; auto &i : obj){} ]",
+			"Attributes: no_unique_address, likely, unlikely",
+			"Packs in Lambda capture templates [ auto x = []<typename ...T>(T ...t){}; ]",
+			"Coroutines (co_await, co_yeild, co_return)",
+			"Modules [ export module projname; import <iostream>; export void fnc(){} ||| import projname; int main(){fnc();}]"
 		};
 
 		std::cout << "List of Stuff:\n\n";
 		Func(Items);
+
 	}
 
 	void Func(std::vector<std::string> strings) {
