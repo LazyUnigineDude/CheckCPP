02d34ae8a7311af18eb7a1ac551d9c5064514000
LazyUnigineDude <lazy.unigine@gmail.com>
2023-02-08 14:35:06 -0500

    Add

--- CXX14.h
+++ CXX14.h
@@ -7,10 +7,11 @@ struct CXX14 {
 	CXX14() {
 		std::vector<std::string> Items = {
 		"Variable Templates [ template<class T> T obj = var; ]",
-		"Generic Lambda + Capture [ auto x = [/*&*/ this /*=*/](){}; ]",
+		"Generic Lambda Capture clauses [ auto x = [/*&*/ this /*=*/](){}; ]",
 		"Binary Literals [ 0x2a, 0b10010 ]",
 		"Make uniques [ std::unique_ptr<type> var = std::make_unique<type>(value); ]",
-		"Shared_Lock"
+		"Shared_Lock",
+		"Aggregate Classes [ class Foo{int x, y, z[2]; }; int main(){ Foo x = {1, 2 , {3,4} }; } ]"
 		};
 
 		std::cout << "List of Stuff:\n\n";
